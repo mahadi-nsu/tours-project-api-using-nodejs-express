b7ca6db9a7bfd21cc68efb47d3c2424488592e61
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-03-04 13:41:21 +0600

    some more refactoring to the code

--- app.js
+++ app.js
@@ -100,12 +100,22 @@ const deleteTour = (req,res)=>{
 }
 
 // All routes
-app.get('/api/v1/tours',getAllTours);
-app.get('/api/v1/tours/:id',getTour);
-app.post('/api/v1/tours',createTour);
-app.patch('/api/v1/tours/:id',updateTour);
-app.delete('/api/v1/tours/:id',deleteTour);
+// app.get('/api/v1/tours',getAllTours);
+// app.get('/api/v1/tours/:id',getTour);
+// app.post('/api/v1/tours',createTour);
+// app.patch('/api/v1/tours/:id',updateTour);
+// app.delete('/api/v1/tours/:id',deleteTour);
 
+//route add
+app
+   .route('/api/v1/tours')
+   .get(getAllTours)
+   .post(createTour);
+app
+   .route('/api/v1/tours/:id')
+   .get(getTour)
+   .patch(updateTour)
+   .delete(deleteTour)
 
 //server
 app.listen(port, () => {
