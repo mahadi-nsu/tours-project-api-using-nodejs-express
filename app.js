79330a49c42eb959d48b3789247abf458216418e
Mahadi Hassan <mahadi.hassan@northsouth.edu>
2020-08-04 14:15:00 +0600

    eparet file added for server

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
+
 const express = require('express');
-const fs = require('fs');
 const morgan = require('morgan');
-var path = require('path');
 
-
+//declare the app
 const app = express();
-const port = 3000;
 
 //add json parsing middleware
 app.use(express.json());
 app.use(morgan('dev'));
-
-
-//adding custom middleware
-app.use((req, res, next) => {
-    console.log("hello from middleware");
-    // req.requestTime
-    next();
-});
 
 //middleware to show the request time
 app.use((req, res, next) => {
@@ -34,7 +24,4 @@ const userRouter = require('./routes/userRoutes');
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-//server
-app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
-})
+module.exports = app;
