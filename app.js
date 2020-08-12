fc663769f56c3c472766d18c30239089f0f126b0
Mahadi Hassan <mahadi.hassan@northsouth.edu>
2020-08-12 14:44:44 +0600

    development mode defiend

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const app = express();
 
 //add json parsing middleware
 app.use(express.json());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV == 'development') {
+    app.use(morgan('dev'));
+}
 
 // serve static files
 app.use(express.static(`${__dirname}/public`));
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const dotenv = require('dotenv');
-const app = require('./app');
-
 dotenv.config({
     path: './config.env'
 })
+const app = require('./app');
+
+// dotenv.config({
+//     path: './config.env'
+// })
 console.log(process.env.NODE_ENV);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // console.log(app.get('env'));
 
